e23c9db1dff5e3038d3a3553a3b8c2ac1c440cc8
Vojtěch Šilhánek <148778457+vojtechsilhanek@users.noreply.github.com>
2024-12-30 12:06:13 +0100

    Add files via upload

--- code.js
+++ code.js
@@ -1,12 +1,15 @@
 let rows = 24;
 let cols = 24;
+let playing = false;
 
 
 
 
 
 document.addEventListener("DOMContentLoaded", function() {
+    setupControlButtons();
     createTable();
+    
 })
 // initialize
 
@@ -26,12 +29,51 @@ function createTable() {
             let cell = document.createElement("td");
             cell.setAttribute("id", i + "_" + j);
             cell.setAttribute("class", "dead");
+            cell.onclick = cellClickHandler;
             tr.appendChild(cell);
         }
         table.appendChild(tr);
     }
     gridContainer.appendChild(table);
-}
+};
 
 
+function cellClickHandler() {
+    let classes = this.getAttribute('class');
+    if (classes.indexOf('live') > -1) {
+      this.setAttribute('class', 'dead');
+    } else {
+      this.setAttribute('class', 'live');
+    }
+  };
+  
+
+  function setupControlButtons() {
+    let startButton = document.querySelector('#start');
+    let clearButton = document.querySelector('#clear');
+    startButton.onclick = () => {
+        if (playing) {
+            console.log('Pause the Game');
+            playing = false;
+            startButton.innerHTML = 'continue';
+          } else {
+            console.log('Cont the game');
+            playing = true;
+            startButton.innerHTML = 'pause';
+            play();
+          }
+    };
+    clearButton.onclick = function() {
+        playing = false;
+        startButton.innerHTML = "start";
+    };
+  };
+
+  function play() {
+    console.log("Play the game")
+};
+
+
+
+    
 // start everything
